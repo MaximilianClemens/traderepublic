db3af42d3488d22926b9d36ad8d580f5244869d5
Maximilian Clemens <maximilian.clemens@mailbox.org>
2024-09-22 18:41:57 +0200

    get transactions

--- test.py
+++ test.py
@@ -11,4 +11,5 @@ if client.login(os.getenv('TR_NUMBER'), os.getenv('TR_PIN')):
     client.auth(two_factor)
 
 if client.logged_in:
+    print(client.get_timeline_transactions())
     client.logout()
--- traderepublic/client.py
+++ traderepublic/client.py
@@ -2,6 +2,7 @@ import requests
 import websocket
 import threading
 import json
+import time
 
 from . import VERSION
 
@@ -28,6 +29,9 @@ class Client:
 
         self.login_processid = None
         self.logged_in = False
+        self.websocket_connected = False
+
+        self.message_dict = {}
 
     def _json_api(self, *args):
         response = self._api(*args)
@@ -48,8 +52,6 @@ class Client:
             'pin': str(pin)
         })
 
-        print(response)
-
         if 'processId' in response:
             # success
             self.login_processid = response['processId']
@@ -59,14 +61,20 @@ class Client:
             return False
 
     def auth(self, two_factor: int | str) -> bool:
-        print('auth')
         if not self.login_processid:
             # Todo: raise error
             return False
 
         response = self._api('POST', f'/v1/auth/web/login/{self.login_processid}/{two_factor}')
-        print(response)
         self._websocket_start()
+        
+        for i in range(0,30):
+            if self.websocket_connected:
+                break
+            time.sleep(1)
+
+        if not self.websocket_connected:
+            raise Exception('ws not connected')
 
         self.logged_in = True
 
@@ -96,9 +104,21 @@ class Client:
 
     def _websocket_on_close(self, wsapp: websocket.WebSocketApp, close_status_code: int, close_msg: str):
         print(f'WS ! CLOSE, code: {close_status_code}, message: {close_msg}')
+        self.websocket_connected = False
+        self.logout(False)
+
         
     def _websocket_on_message(self, wsapp: websocket.WebSocketApp, message: str):
         print(f'WS > {message}')
+        if message == 'connected':
+            self.websocket_connected = True
+        else:
+            (id, data) = message.split(' ', 1)
+            if id not in self.message_dict:
+                self.message_dict[id] = []
+
+            self.message_dict[id].append(data)
+
 
     def _websocket_send(self, function:str, number:int | None, message:str | dict | None = None):
         if not number:
@@ -115,16 +135,56 @@ class Client:
 
         print(f'WS < {send_message}')
         self.ws.send(send_message)
+        return str(number)
         
+    def _websocket_send_sub(self, data:dict, end=True):
+        # ToDo: make blocking optional, if we have a usecase for that
+        id = self._websocket_send('sub', None, data)
+        
+        for i in range(0,30):
+            time.sleep(1)
+            if id in self.message_dict:
+                break
+        
+        if id not in self.message_dict:
+            raise Exception('send sub Timeout')
 
-    def logout(self) -> bool:
+        time.sleep(1) #fixme
+        response = []
+        for message in self.message_dict[id]:
+            s = message.split(' ', 1)
+            if len(s) == 1:
+                s.append(None)
+            (mode, data) = s
+            if mode == 'A':
+                # Answer?
+                if data:
+                    response.append(json.loads(data))
+            elif mode == 'C':
+                # Close?
+                end = True
+
+        if end:
+            self._websocket_send('unsub', id)
+
+        return response
+
+
+    def get_timeline_transactions(self):
+        datas = self._websocket_send_sub({
+            'type': 'timelineTransactions'
+        })
+        for data in datas:
+            for item in data['items']:
+                print(f'{item["id"]} {item["eventType"]} {item["title"]} {item["amount"]["value"]} {item["amount"]["currency"]}')
+
+    def logout(self, close_socket:bool = True) -> bool:
         if not self.logged_in:
             return False
         
         response = self._api('POST', f'/v1/auth/web/logout')
-        print(response)
-        print(response.text)
-        self.ws.close()
+        if close_socket:
+            self.ws.close()
 
         self.logged_in = False
         return True
